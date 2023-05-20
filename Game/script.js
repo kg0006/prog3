723d7e9f10b7820ce924cafed4543336c93450c3
kg0006 <glutsanyan72@gmail.com>
2023-05-20 18:15:51 +0400

    exanaknery

--- Game/script.js
+++ Game/script.js
@@ -1,9 +1,7 @@
-
-
 var side = 20;
-var socket  =   io();
+var socket = io();
 function setup() {
-// console.log("inside setip")
+    // console.log("inside setip")
     // function matrixGenerator(matrixSize, grassCount, grassEaterCount, hunterCount, predatorCount,livingcreatureCount) {
     //     for (let i = 0; i < matrixSize; i++) {
     //     matrix[i] = []
@@ -31,44 +29,69 @@ function setup() {
     //         let y = Math.floor(random(matrixSize));
     //         matrix[y][x] = 4;
     //     }
-        
+
     // }
     //    matrixGenerator(50, 50, 20, 20, 20);
 
 
-    
-   createCanvas(50 * side, 50 * side);
-   background('#acacac');
+
+    createCanvas(50 * side, 50 * side);
+    background('#acacac');
 }
 
 
-  function state (data){
-      const forState =  document.getElementById("state")
-      const forState1 =  document.getElementById("state1")
+function state(data) {
+    const forState = document.getElementById("state")
+    const forState1 = document.getElementById("state1")
 
-      const forState2 =  document.getElementById("state2")
+    const forState2 = document.getElementById("state2")
 
-      const forState3 =  document.getElementById("state3")
+    const forState3 = document.getElementById("state3")
 
-      forState.innerHTML = "GrassCount:" + data.grassCount
+    forState.innerHTML = "GrassCount:" + data.grassCount
 
-      forState1.innerHTML = "grassEaterCount :" + data.grassEaterCount 
+    forState1.innerHTML = "grassEaterCount :" + data.grassEaterCount
 
-      forState2.innerHTML = "hunterCount :" + data.hunterCount 
+    forState2.innerHTML = "hunterCount :" + data.hunterCount
 
-      forState3.innerHTML = "predatorCount :" + data.predatorCount 
+    forState3.innerHTML = "predatorCount :" + data.predatorCount
     // console.log(data);
-  }
+}
+let sum = document.getElementById("summer")
+let aut = document.getElementById("autumn")
+let win = document.getElementById("winter")
+let spr = document.getElementById("spring")
 
+let colors = "green"
+function changeSeason() {
+    colors = "green"
 
+}
+function changeSeason1() {
+    colors = "orange"
+
+}
+function changeSeason2() {
+    colors = "white"
+
+}
+function changeSeason3() {
+    colors = "pink"
+
+}
+
+sum.addEventListener("click",changeSeason)
+aut.addEventListener("click",changeSeason1)
+win.addEventListener("click",changeSeason2)
+spr.addEventListener("click",changeSeason3)
 
 function draww(matrix) {
-// console.log(matrix)
+    // console.log(matrix)
     for (var y = 0; y < matrix.length; y++) {
         for (var x = 0; x < matrix[y].length; x++) {
- 
+
             if (matrix[y][x] == 1) {
-                fill("green");
+                fill(colors);
                 rect(x * side, y * side, side, side);
             }
             else if (matrix[y][x] == 0) {
@@ -93,7 +116,7 @@ function draww(matrix) {
             }
         }
     }
-    
+
 
 
 
@@ -113,18 +136,18 @@ function draww(matrix) {
     //     predator[i].eat();
     // }
 
- }
- 
- socket.on("send massege",draww)
-    socket.on("creatures count", state);
+}
 
- const btn = document.querySelector('button');
+socket.on("send massege", draww)
+socket.on("creatures count", state);
+
+const btn = document.querySelector('button');
 
 function randomColor(number) {
-  return Math.floor(Math.random() * (number+1));
+    return Math.floor(Math.random() * (number + 1));
 }
 
 btn.addEventListener('click', () => {
-  const rndCol = `rgb(${randomColor(255)}, ${randomColor(255)}, ${randomColor(255)})`;
-  document.body.style.backgroundColor = rndCol;
+    const rndCol = `rgb(${randomColor(255)}, ${randomColor(255)}, ${randomColor(255)})`;
+    document.body.style.backgroundColor = rndCol;
 });
